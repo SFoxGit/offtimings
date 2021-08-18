1bfae385dfea978986cf4ac35291f3fa820e34f2
Shawn Fox <foxeasttrojan06@gmail.com>
2021-08-18 16:20:28 -0400

    primary select for BCD

--- src/components/Table/table.jsx
+++ src/components/Table/table.jsx
@@ -6,15 +6,44 @@ import { Container, Row, Button, Col } from 'react-bootstrap';
 export default function AttacksTable() {
   const [powers, setPowers] = useState([])
   const [archtype, setArchtype] = useState()
+  const [primary, setPrimary] = useState()
 
   const selectArchtype = (filter) => {
+    setPrimary()
     console.log(filter)
     let arr = attacks.filter(function(power) {
       return power[13].includes(filter)
     })
-    console.log(arr)
     setPowers(arr)
+    setArchtype(filter)
   }
+
+  const selectPrimary = (filter) => {
+    if (!primary) {
+
+      let arr = powers.filter(function(power) {
+        return power[8] === filter
+      })
+      console.log(arr)
+      setPowers(arr)
+      setPrimary(filter)
+    } else {
+      let arr = attacks.filter(function(power) {
+        return power[13].includes(archtype)
+      })
+      setPowers(arr)
+      let filterArr = arr.filter(function(power) {
+        return power[8] === filter
+      })
+      setPowers(filterArr)
+      setPrimary(filter)
+    }
+  }
+
+  const selectSecondary = (filter) => {
+
+  }
+
   useEffect(() => {
     setPowers(attacks)
   }, [setPowers])
@@ -29,6 +58,28 @@ export default function AttacksTable() {
           <Button size="lg" variant="primary" onClick={() => selectArchtype("controller")}>Controller</Button>
         </Col>
       </Row>
+      {archtype === "blaster" || archtype === "corruptor" || archtype === "defender" ? 
+      <Row>
+        <Col>
+        <Button size="lg" variant="secondary" onClick={() => selectPrimary("Archery")}>Archery</Button>
+        <Button size="lg" variant="secondary" onClick={() => selectPrimary("AssaultRiffle")}>Assault Rifle</Button>
+        <Button size="lg" variant="secondary" onClick={() => selectPrimary("Beam")}>Beam Rifle</Button>
+        <Button size="lg" variant="secondary" onClick={() => selectPrimary("Dark")}>Dark</Button>
+        <Button size="lg" variant="secondary" onClick={() => selectPrimary("DP")}>Dual Pistols</Button>
+        <Button size="lg" variant="secondary" onClick={() => selectPrimary("Elec")}>Elec Blast</Button>
+        <Button size="lg" variant="secondary" onClick={() => selectPrimary("Energy")}>Energy</Button>
+        <Button size="lg" variant="secondary" onClick={() => selectPrimary("Fire")}>Fire</Button>
+        <Button size="lg" variant="secondary" onClick={() => selectPrimary("Ice")}>Ice</Button>
+        <Button size="lg" variant="secondary" onClick={() => selectPrimary("Psy")}>Psychic</Button>
+        <Button size="lg" variant="secondary" onClick={() => selectPrimary("RadBlast")}>Rad Blast</Button>
+        <Button size="lg" variant="secondary" onClick={() => selectPrimary("Sonic")}>Sonic Blast</Button>
+        <Button size="lg" variant="secondary" onClick={() => selectPrimary("Water")}>Water</Button>
+        </Col>
+      </Row> 
+      : 
+      null
+      }
+      
       <Table striped bordered hover variant="dark">
         <thead>
           <tr>
