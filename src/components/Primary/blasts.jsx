cf13fdc8a489994bdf4b377be50d3e064a46d725
Shawn Fox <foxeasttrojan06@gmail.com>
2021-08-19 17:38:18 -0400

    button size dom vs cont

--- src/components/Primary/blasts.jsx
+++ src/components/Primary/blasts.jsx
@@ -7,19 +7,19 @@ export default function Blasts(props) {
   return (
     <Row className="justify-content-around mt-3 bg-dark p-3 customBoxShadow">
       <Col xs={12} className="text-white fw-bolder mb-2 border-bottom"><h3>Blast Type</h3></Col>
-        <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("Archery")}>Archery</Button></Col>
-        <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("AssaultRiffle")}>Assault Rifle</Button></Col>
-        <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("Beam")}>Beam Rifle</Button></Col>
-        <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("Dark")}>Dark</Button></Col>
-        <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("DP")}>Dual Pistols</Button></Col>
-        <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("Elec")}>Elec Blast</Button></Col>
-        <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("Energy")}>Energy</Button></Col>
-        <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("Fire")}>Fire</Button></Col>
-        <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("Ice")}>Ice</Button></Col>
-        <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("Psy")}>Psychic</Button></Col>
-        <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("RadBlast")}>Rad Blast</Button></Col>
-        <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("Sonic")}>Sonic Blast</Button></Col>
-        <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("Water")}>Water</Button></Col>
+        <Col xs={6} md={3}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("Archery")}>Archery</Button></Col>
+        <Col xs={6} md={3}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("AssaultRiffle")}>Assault Rifle</Button></Col>
+        <Col xs={6} md={3}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("Beam")}>Beam Rifle</Button></Col>
+        <Col xs={6} md={3}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("Dark")}>Dark</Button></Col>
+        <Col xs={6} md={3}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("DP")}>Dual Pistols</Button></Col>
+        <Col xs={6} md={3}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("Elec")}>Elec Blast</Button></Col>
+        <Col xs={6} md={3}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("Energy")}>Energy</Button></Col>
+        <Col xs={6} md={3}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("Fire")}>Fire</Button></Col>
+        <Col xs={6} md={3}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("Ice")}>Ice</Button></Col>
+        <Col xs={6} md={3}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("Psy")}>Psychic</Button></Col>
+        <Col xs={6} md={3}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("RadBlast")}>Rad Blast</Button></Col>
+        <Col xs={6} md={3}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("Sonic")}>Sonic Blast</Button></Col>
+        <Col xs={6} md={3}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("Water")}>Water</Button></Col>
     </Row>
   )
 }
--- src/components/Primary/controls.jsx
+++ src/components/Primary/controls.jsx
@@ -4,19 +4,23 @@ import { Row, Button, Col } from 'react-bootstrap';
 export default function Controls(props) {
   const selectPrimary = props.selectPrimary
   const archtype = props.archtype
+  let buttonSize = 4
+  if (archtype === 'dom') {
+    buttonSize = 3
+  }
 
   return (
     <Row className="justify-content-around mt-3 bg-dark p-3 customBoxShadow">
       <Col xs={12} className="text-white fw-bolder mb-2 border-bottom"><h3>Control Type</h3></Col>
-      <Col xs={6} md={4}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("FireControl")}>Fire</Button></Col>
-      <Col xs={6} md={4}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("EarthControl")}>Earth</Button></Col>
-      {archtype === "controller" ? <Col xs={6} md={4}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("IllControl")}>Illusion</Button></Col> : null}
-      <Col xs={6} md={4}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("IceControl")}>Ice</Button></Col>
-      <Col xs={6} md={4}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("GravControl")}>Grav</Button></Col>
-      <Col xs={6} md={4}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("DarkControl")}>Dark</Button></Col>
-      <Col xs={6} md={4}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("ElecControl")}>Elec</Button></Col>
-      <Col xs={6} md={4}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("MindControl")}>Mind</Button></Col>
-      <Col xs={6} md={4}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("PlantControl")}>Plant</Button></Col>
+      <Col xs={6} md={buttonSize}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("FireControl")}>Fire</Button></Col>
+      <Col xs={6} md={buttonSize}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("EarthControl")}>Earth</Button></Col>
+      {archtype === "controller" ? <Col xs={6} md={buttonSize}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("IllControl")}>Illusion</Button></Col> : null}
+      <Col xs={6} md={buttonSize}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("IceControl")}>Ice</Button></Col>
+      <Col xs={6} md={buttonSize}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("GravControl")}>Grav</Button></Col>
+      <Col xs={6} md={buttonSize}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("DarkControl")}>Dark</Button></Col>
+      <Col xs={6} md={buttonSize}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("ElecControl")}>Elec</Button></Col>
+      <Col xs={6} md={buttonSize}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("MindControl")}>Mind</Button></Col>
+      <Col xs={6} md={buttonSize}><Button size="lg" className="w-100 my-2 fw-bolder" variant="secondary" onClick={() => selectPrimary("PlantControl")}>Plant</Button></Col>
     </Row>
   )
 }
