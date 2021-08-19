c763e041059f489057c0a5fc9522bd68e90419cf
Shawn Fox <foxeasttrojan06@gmail.com>
2021-08-18 21:42:10 -0400

    corr secondary info

--- src/components/Secondary/corruptor.jsx
+++ src/components/Secondary/corruptor.jsx
@@ -6,10 +6,10 @@ export default function CorruptorSecondaries(props) {
 
   return (
     <Row className="justify-content-around m-2 bg-dark p-3">
-            <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" onClick={() => selectSecondary("Poison")}>Poison</Button></Col>
-            <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" onClick={() => selectSecondary("RadEmission")}>Rad</Button></Col>
-            <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" onClick={() => selectSecondary("Nature")}>Nature</Button></Col>
-            <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" onClick={() => selectSecondary("other")}>Other</Button></Col>
+            <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" variant="info" onClick={() => selectSecondary("Poison")}>Poison</Button></Col>
+            <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" variant="info" onClick={() => selectSecondary("RadEmission")}>Rad</Button></Col>
+            <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" variant="info" onClick={() => selectSecondary("Nature")}>Nature</Button></Col>
+            <Col xs={6} md={2}><Button size="lg" className="w-100 my-2 fw-bolder" variant="info" onClick={() => selectSecondary("other")}>Other</Button></Col>
         </Row>
   )
 }
