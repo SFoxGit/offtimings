25ca8b036d895cad94d00a8467c0aac21fa8c87d
sfoxgit <sfoxgit@gmail.com>
2021-08-22 02:38:33 -0400

    object modifiers

--- src/modifiers.js
+++ src/modifiers.js
@@ -1,17 +1,17 @@
-const modifiers = [	
-{Blas: 76.9134},
-{Cont: 51.2809},
-{Def: 56.4132},
-{Scra: 31.9052},
-{Tank: 35.2552},
-{Khel: 53.3852},
-{Sent: 45.5837},
-{Bru: 37.6097},
-{Corr: 56.4132},
-{Dom: 73.9593},
-{MM: 46.1537},
-{Sta: 33.8449},
-{SoA: 56.4132},
-]
+const modifiers = {	
+Blas: 76.9134,
+Cont: 51.2809,
+Def: 56.4132,
+Scra: 31.9052,
+Tank: 35.2552,
+Khel: 53.3852,
+Sent: 45.5837,
+Bru: 37.6097,
+Corr: 56.4132,
+Dom: 73.9593,
+MM: 46.1537,
+Sta: 33.8449,
+SoA: 56.4132,
+}
 
 export default modifiers;
